a38a0b8e0aade59bf4e87137ff35b755d7edd072
RJ Hallsted <rhallste@student.42.us.org>
2017-09-18 14:33:17 -0700

    Wrote some more little functions

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,25 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/18 11:31:58 by rhallste          #+#    #+#             */
-/*   Updated: 2017/09/18 14:31:36 by rhallste         ###   ########.fr       */
+/*   Created: 2017/09/18 14:28:56 by rhallste          #+#    #+#             */
+/*   Updated: 2017/09/18 14:29:17 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-void ft_putchar(char c);
-void ft_putchar(char c, int fd);
-void ft_putendl(char const *s);
-void ft_putendl_fd(char const *s, int fd);
-void ft_putnbr(int n);
-void ft_putnbr_fd(int n, int fd);
-void ft_putstr(char const *s);
-void ft_putstr_fd(char const *s, int fd);
-
-#endif
+void	ft_putendl_fd(char const *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar('\n');
+}
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,25 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/18 11:31:58 by rhallste          #+#    #+#             */
-/*   Updated: 2017/09/18 14:31:36 by rhallste         ###   ########.fr       */
+/*   Created: 2017/09/18 14:30:33 by rhallste          #+#    #+#             */
+/*   Updated: 2017/09/18 14:31:10 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-void ft_putchar(char c);
-void ft_putchar(char c, int fd);
-void ft_putendl(char const *s);
-void ft_putendl_fd(char const *s, int fd);
-void ft_putnbr(int n);
-void ft_putnbr_fd(int n, int fd);
-void ft_putstr(char const *s);
-void ft_putstr_fd(char const *s, int fd);
-
-#endif
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("2147483648", fd);
+	}
+	else
+	{
+		if (n < 0)
+		{
+			n *= -1;
+			ft_putchar_fd('-', fd);
+		}
+		ft_putchar_fd((n % 10) + '0', fd);
+		if (nb > 9)
+		{
+			ft_putnbr_fd(n / 10, fd);
+		}
+	}
+}
