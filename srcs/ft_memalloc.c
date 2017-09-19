56f3122b416c82227f2523d980c9e209566b5f22
RJ Hallsted <rhallste@student.42.us.org>
2017-09-19 15:14:12 -0700

    Wrote ft_memtset && ft_memalloc

---
+++ srcs/ft_memalloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/19 14:59:25 by rhallste          #+#    #+#             */
+/*   Updated: 2017/09/19 15:10:47 by rhallste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "../libft.h"
+
+void *ft_memalloc(size_t size)
+{
+	void *new;
+
+	if(!(new = malloc(size)))
+		return (NULL);
+	ft_memset(new, 0, size);	
+	return (new);
+}
