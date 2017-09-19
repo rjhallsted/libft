08bf2cdf5f1afc116766f57260827d4509e02589
RJ Hallsted <rhallste@student.42.us.org>
2017-09-19 14:55:24 -0700

    Fixed len=0 bug in strncmp

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/19 11:44:10 by rhallste          #+#    #+#             */
-/*   Updated: 2017/09/19 11:50:54 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/09/19 14:53:41 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	if (n == 0)
+		return (0);
 	while (*s1 && *s2 && *s1 == *s2 && n > 0)
 	{
 		s1++;
