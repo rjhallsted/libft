cefb5ac80f030a3cdd2cf9df687a8681e3abba79
RJ Hallsted <rhallste@student.42.us.org>
2017-09-21 11:25:58 -0700

    Fixed ft_memmove

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,31 +6,27 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/19 18:55:02 by rhallste          #+#    #+#             */
-/*   Updated: 2017/09/20 12:06:02 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/09/21 11:25:04 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
-
-static int	overlaps(void *dst, const void *src, size_t i, size_t len)
-{
-	return (dst + i >= src && dst + i <= src + len - 1);
-}
+#include "../libft.h"
 
 void		*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t		i;
-	char		*dst_str;
-	const char	*src_str;
+	void *save;
 
-	dst_str = dst;
-	src_str = src;
-	i = 0;
-	while (i < len)
+	save = dst;
+	if (dst > src && dst - src < (long)len)
 	{
-		if (!overlaps(dst, src, i, len))
-			dst_str[i] = src_str[i];
-		i++;
+		while (len > 0)
+		{
+			*(char *)(dst + len - 1) = *(char *)(src + len - 1);
+			len--;
+		}
 	}
-	return (dst);
+	else
+		ft_memcpy(dst, src, len);
+	return (save);
 }
