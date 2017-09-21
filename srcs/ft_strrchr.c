2d84af1169b19354cf680928a715115e17aa092c
RJ Hallsted <rhallste@student.42.us.org>
2017-09-20 18:29:35 -0700

    fixed ft_strrchr

--- srcs/ft_strrchr.c
+++ srcs/ft_strrchr.c
@@ -6,14 +6,14 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/20 09:33:18 by rhallste          #+#    #+#             */
-/*   Updated: 2017/09/20 09:41:57 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/09/20 18:28:19 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include "../libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
 	char *last;
 
