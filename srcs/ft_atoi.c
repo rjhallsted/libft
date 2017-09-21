a7cfa1f94546ed1327544ae86fb2623f69ae46f3
RJ Hallsted <rhallste@student.42.us.org>
2017-09-21 14:36:21 -0700

    fix ft_atoi

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/18 19:36:59 by rhallste          #+#    #+#             */
-/*   Updated: 2017/09/21 14:20:09 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/09/21 14:35:23 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,32 @@
 
 static int	to_number(const char *str)
 {
-	int nb;
+	long nb;
 
 	nb = 0;
 	while (ft_isdigit(*str))
 	{
+		if (nb * 10 < 0)
+			return (-1);
 		nb = (nb * 10) + (*str - '0');
 		str++;
 	}
-	return (nb);
+	return ((int)nb);
 }
 
 int			ft_atoi(const char *str)
 {
+	int a;
+
+	a = 0;
 	while (ft_isspace(*str))
 		str++;
 	if (*str == '+')
 		str++;
 	else if (*str == '-')
-		return (-1 * to_number(++str));
+	{
+		a = to_number(++str);
+		return ((-1 * a > 0) ? 0 : -1 * a);
+	}
 	return (to_number(str));
 }
