a1748e16ec750f5e20d2844b3ab5b5090d55de95
RJ Hallsted <rhallste@student.42.us.org>
2017-09-25 17:05:52 -0700

    possibly fixed bug in strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/20 09:10:54 by rhallste          #+#    #+#             */
-/*   Updated: 2017/09/25 16:51:36 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/09/25 17:05:33 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t dst_len;
 
 	dst_len = ft_strlen(dst);
-	if (dst_len <= size)
+	if (dst_len < size)
 	{
 		ft_strncat(dst, src, size - dst_len - 1);
 		return (dst_len + ft_strlen(src));
