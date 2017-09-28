2eaab9eacb590d258ff5056373a32c5357d9382d
RJ Hallsted <rhallste@student.42.us.org>
2017-09-28 14:16:01 -0700

    Removed a memory leak from ft_lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/22 20:37:17 by rhallste          #+#    #+#             */
-/*   Updated: 2017/09/23 23:17:15 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/09/28 14:15:02 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,11 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 		return (new);
 	}
 	else if (!(new->content = (void *)malloc(content_size)))
+	{
+		free(new);
+		new = NULL;
 		return (NULL);
+	}
 	ft_memcpy(new->content, content, content_size);
 	new->content_size = content_size;
 	new->next = NULL;
